6c5372774d6ce17069216022a7d5d4e21a477df6
Choo Ying Juan <33310617+YJCHOO@users.noreply.github.com>
2022-08-23 19:42:09 +0800

    Initial Commit

---
+++ CryptoPriceMenuBar/CryptoPriceMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceMenuBarApp.swift
+//  CryptoPriceMenuBar
+//
+//  Created by Juan on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
